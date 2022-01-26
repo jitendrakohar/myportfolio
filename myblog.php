a74b95da6bdd1986a024ad42a2bf688c513a251b
jitendra kohar <jitendrakohar05@gmail.com>
2022-01-26 21:30:49 +0530

    this  is the repositories

---
+++ myblog.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>my Blog</title>
+</head>
+
+<body>
+    <div class="container">
+        <div class="aside">
+            This is the aside section where the blog and all the buton is stored
+        </div>
+
+        <div class="maincontent">
+            This is the main section of the blog where all the contenet of the blog is stored
+        </div>
+
+        <div class="leftside">
+            This is the lefft side of the blog where the related content of the blog is stored
+        </div>
+    </div>
+</body>
+
+</html>
